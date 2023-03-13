6d3db4961d62fe3a733c141d7834ade2fb0f8faf
Florencio Britez <florenciobritez49@gmail.com>
2023-03-12 23:56:03 -0300

    Add routes

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue"
+import Login from "../pages/Login.vue"
+import Register from "../pages/Register.vue"
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
+   
+]
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
